c010a22f396d255911d32b2d77172cb4daa24968
enescanguven <enescanguven@gmail.com>
2021-03-29 21:46:33 +0300

    until function replaced by while

--- bot.py
+++ bot.py
@@ -2,11 +2,14 @@ import webbrowser
 import time
 from datetime import datetime
 import json
-now = datetime.datetime.now()
+
+monday = datetime.now().day-datetime.today().weekday()
+
 
 json_file = open('conf.json')
 chrome_path = '/usr/bin/google-chrome %s'
 data = json.load(json_file)
+
 class Meeting:
   def __init__(self, i ):
     self.name = data["courses"][i]["name"]
@@ -21,16 +24,30 @@ def get_courses(i):
   course = Meeting(i)
   return course
 
-for i in range(number_of_courses):
+def check_time(day, hour, mint, course_day, course_hour, course_min):
+  """ print(day, "bu gün")
+  print(course_day, "kurs gün") """
+  if course_day == day:
+    print("1")
+    if course_hour == hour:
+      print("2")
+      if course_min <= mint:
+        print("3")
+        return True
+  return False
 
-  m = get_courses(i)
-  
-  to_day = datetime.datetime.today().weekday()
+while True:
+  for i in range(number_of_courses):
+    day = datetime.today().weekday()
+    hour = datetime.now().hour
+    mint = datetime.now().minute
+    m = get_courses(i)
+    a=check_time(day, hour, mint, m.day, m.hour, m.mint)
+    if a == True:
+      print ("bağlanılıyor", m.name)
+      webbrowser.get(chrome_path).open(m.url)
+      exit()
 
-  
-#print(an.day)
+  time.sleep(10)
+print("deneme")
 
-#if an.hour >=3:    
- # print("connecting")
-  #webbrowser.get(chrome_path).open(m1.url)
-  #time.sleep()
